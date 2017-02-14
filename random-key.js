0ea47bbf8353a29966be375c08337189f85ff005
Jollr <jolle.lont@gmail.com>
2017-02-14 22:18:32 +0100

    implement random key

--- random-key.js
+++ random-key.js
@@ -1,3 +1,10 @@
 $(function() {
-	$('#main').html('test');
+	var getRandomInt = function(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	var keys = [ 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+	var randomKey = keys[getRandomInt(0, 11)];
+
+	$('#main').html(randomKey);
 });
